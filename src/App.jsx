f8d3762c98364cb3929b7292da50c5a2b82e3f31
farhan-genius <sfarhan316@gmail.com>
2026-04-22 15:25:57 +0500

    color list

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App({colors}) {
   return (
     <>
-    
-    </>
+    <p>
+      {colors.map((value) => (
+        <p style={{color: value}}>
+          {value}
+        </p>
+      ))
+    }
+    </p>
+  </>
   )
+  
 }
-
-export default App
